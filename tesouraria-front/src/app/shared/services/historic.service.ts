ffa4db3f3c7a2eb850f5d1c4fff3d586458bfed0
Samuel Souza <samuelgsete@gmail.com>
2021-01-05 15:28:22 -0300

    Leanding page e atualização no path das API

--- tesouraria-front/src/app/shared/services/historic.service.ts
+++ tesouraria-front/src/app/shared/services/historic.service.ts
@@ -4,13 +4,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class HistoricService {
 
-    private urlBase: string = 'http://localhost:3000/historic';
+    private urlBase: string = 'http://localhost:3000/treasury';
 
     public constructor(private readonly http: HttpClient) {}
 
-    public getHistoric(treasuryId: number, year: number) {
+    public getHistoric(id: number, year: number) {
         const _params = new HttpParams().set('year', `${year}`);
 
-        return this.http.get<any>(this.urlBase.concat(`/${treasuryId}`), { observe: 'response', params: _params });
+        return this.http.get<any>(this.urlBase.concat(`/${id}/historic`), { observe: 'response', params: _params });
     }
 }
--- tesouraria-front/src/app/shared/services/report.service.ts
+++ tesouraria-front/src/app/shared/services/report.service.ts
@@ -4,18 +4,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ReportService {
 
-    private urlBase: string = 'http://localhost:3000/report';
+    private urlBase: string = 'http://localhost:3000/treasury';
 
     public constructor(private readonly http: HttpClient) {}
 
     public getReport(id: number, year: number, month: number) {
         const _params = new HttpParams().set('year', `${year}`).set('month', `${month}`);
 
-        return this.http.get<any>(this.urlBase.concat(`/${id}`), { observe: 'response', params: _params });
+        return this.http.get<any>(this.urlBase.concat(`/${id}/report`), { observe: 'response', params: _params });
     }
 
     public downloadReport(id: number, year: number, month: number) {
         const _params = new HttpParams().set('year', `${year}`).set('month', `${month}`);
-        return this.http.get<any>(this.urlBase.concat(`/download/${id}`), { responseType: 'text' as 'json', observe: 'response', params: _params });
+        return this.http.get<any>(this.urlBase.concat(`/${id}/report/download`), { responseType: 'text' as 'json', observe: 'response', params: _params });
     }
 }
